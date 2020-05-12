884d9784af77ea1cb0c8d35df169f1fe019a63b9
shayan <shayanpourvatan@gmail.com>
2020-05-12 16:05:07 +0430

    Add receiver for communicate with bazaarApp

---
+++ auth/src/main/java/com/farsitel/bazaar/auth/receiver/AuthReceiver.kt
@@ -0,0 +1,11 @@
+package com.farsitel.bazaar.auth.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AuthReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // TODO handle receive broadcast from cafeBazaarApp
+    }
+}
