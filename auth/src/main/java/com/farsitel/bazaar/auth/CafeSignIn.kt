5546c37d2ec25e1db03216559124b6a44706904d
shayan <shayanpourvatan@gmail.com>
2020-05-17 13:13:32 +0430

    Create callee class for cafeBazaarAuth

---
+++ auth/src/main/java/com/farsitel/bazaar/auth/CafeSignIn.kt
@@ -0,0 +1,10 @@
+package com.farsitel.bazaar.auth
+
+import android.content.Context
+
+object CafeSignIn {
+
+    fun getLastSignedInAccount(context: Context, callback: CafeSingInCallback) {
+
+    }
+}
