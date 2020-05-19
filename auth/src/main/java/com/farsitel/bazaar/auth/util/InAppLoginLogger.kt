0f65a37464f45f0bc8ce3333bf2bdf1bb98f80f4
shayan <shayanpourvatan@gmail.com>
2020-05-19 14:41:45 +0430

    Add logger

---
+++ auth/src/main/java/com/farsitel/bazaar/auth/util/InAppLoginLogger.kt
@@ -0,0 +1,19 @@
+package com.farsitel.bazaar.auth.util
+
+import android.util.Log
+
+object InAppLoginLogger {
+    var mDebugLog = false
+    val mDebugTag = "inAppLogin"
+    fun logDebug(msg: String) {
+        if (mDebugLog) Log.d(mDebugTag, msg)
+    }
+
+    fun logError(msg: String) {
+        Log.e(mDebugTag, "In-app login error: $msg")
+    }
+
+    fun logWarn(msg: String) {
+        Log.w(mDebugTag, "In-app login warning: $msg")
+    }
+}
