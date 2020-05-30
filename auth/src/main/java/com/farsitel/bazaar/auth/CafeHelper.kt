fa8e0873563e48abfc1c4a9f6a2089a77500b7db
shayan <shayanpourvatan@gmail.com>
2020-05-30 16:57:39 +0430

    Check correct bazaar installed on device

--- auth/src/main/java/com/farsitel/bazaar/auth/CafeHelper.kt
+++ auth/src/main/java/com/farsitel/bazaar/auth/CafeHelper.kt
@@ -1,6 +1,7 @@
 package com.farsitel.bazaar.auth
 
 import android.content.Context
+import com.farsitel.bazaar.auth.security.Security
 import com.farsitel.bazaar.auth.util.getPackageInfo
 import com.farsitel.bazaar.auth.util.versionCodeSDKAware
 import com.farsitel.bazaar.auth.view.CafeInstallerActivity
@@ -11,16 +12,17 @@ object CafeHelper {
     private const val BAZAAR_WITH_AUTH_VERSION = 801300
 
     fun isBazaarInstalledOnDevice(context: Context): Boolean =
-        getPackageInfo(context, BAZAAR_PACKAGE_NAME) != null
+        getPackageInfo(context, BAZAAR_PACKAGE_NAME) != null &&
+                Security.verifyBazaarIsInstalled(context)
 
     fun isBazaarNeedToUpdate(context: Context): Boolean =
         getBazaarVersion(context) >= BAZAAR_WITH_AUTH_VERSION
 
-    fun showInstallBazaarViewIfNeeded(context: Context) {
+    fun showInstallBazaarView(context: Context) {
         CafeInstallerActivity.startCafeInstallerActivityForInstallBazaar(context)
     }
 
-    fun showUpdateBazaarViewIfNeeded(context: Context) {
+    fun showUpdateBazaarView(context: Context) {
         CafeInstallerActivity.startCafeInstallerActivityForUpdateBazaar(context)
     }
 
