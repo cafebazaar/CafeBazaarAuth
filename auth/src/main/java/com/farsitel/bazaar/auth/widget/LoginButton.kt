4a644f462ac197707bda0a64ddb22d03cd7c4c6a
shayan <shayanpourvatan@gmail.com>
2020-05-30 14:43:23 +0430

    Add loginButton

---
+++ auth/src/main/java/com/farsitel/bazaar/auth/widget/LoginButton.kt
@@ -0,0 +1,62 @@
+package com.farsitel.bazaar.auth.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.farsitel.bazaar.auth.R
+
+class LoginButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+        val attr = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.LoginButton,
+            defStyleAttr,
+            0
+        )
+
+        val viewSize = LoadingButtonSize.fromXml(
+            attr.getInt(
+                R.styleable.LoginButton_size,
+                LoadingButtonSize.NORMAL.ordinal
+            )
+        )
+
+        attr.recycle()
+        addViewBasedOnViewSize(viewSize)
+    }
+
+    private fun addViewBasedOnViewSize(viewSize: LoadingButtonSize) {
+        removeAllViews()
+        when (viewSize) {
+            LoadingButtonSize.NORMAL -> addViewByLayoutId(R.layout.login_button_normal)
+            LoadingButtonSize.BIG -> addViewByLayoutId(R.layout.login_button_big)
+        }
+    }
+
+    private fun addViewByLayoutId(layoutId: Int) {
+        val view = LayoutInflater.from(context)
+            .inflate(layoutId, this, true)
+
+        addView(view)
+    }
+
+    enum class LoadingButtonSize {
+        NORMAL,
+        BIG;
+
+        companion object {
+            fun fromXml(xmlSize: Int) = when (xmlSize) {
+                0 -> NORMAL
+                1 -> BIG
+                else -> NORMAL
+            }
+        }
+    }
+}
