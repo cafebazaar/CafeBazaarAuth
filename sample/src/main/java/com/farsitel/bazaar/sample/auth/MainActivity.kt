d315eaa52e2f4fd76f5f83f1b8b284cdb7089d7d
Shayan <shayanpourvatan@gmail.com>
2020-12-14 14:05:30 +0330

    Fix typo issue

--- sample/src/main/java/com/farsitel/bazaar/sample/auth/MainActivity.kt
+++ sample/src/main/java/com/farsitel/bazaar/sample/auth/MainActivity.kt
@@ -93,7 +93,7 @@ class MainActivity : AppCompatActivity() {
             )
 
             if (response?.isSuccessful == true) {
-                println("Use already login ${response.data?.accountId}")
+                println("Use already logged in ${response.data?.accountId}")
             }
         }.start()
     }
